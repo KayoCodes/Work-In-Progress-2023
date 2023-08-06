b1963f0f2468ce912bb394b28edba5e74171ab20
jacobschool <brockett04@gmail.com>
2023-08-06 00:55:40 -0500

    created LoginActivity.java and corresponding activity_login.xml; need to add functionality to button, troubleshoot why image does not appear

---
+++ app/src/main/java/com/example/work_in_progress_2023/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.work_in_progress_2023;
+
+import androidx.activity.ComponentActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends ComponentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
