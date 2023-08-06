7225d10f61933de7ff1e48ad45c8d2cd7e575c11
KayoCodes <keenan.ray@my.utsa.edu>
2023-08-06 12:29:29 -0500

    Profile view layout added

--- app/src/main/java/com/example/work_in_progress_2023/LoginActivity.java
+++ app/src/main/java/com/example/work_in_progress_2023/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.work_in_progress_2023;
 
 import androidx.activity.ComponentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends ComponentActivity {
 
@@ -10,5 +13,17 @@ public class LoginActivity extends ComponentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        configureLoginButton();
+    }
+    private void configureLoginButton(){
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, profileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
