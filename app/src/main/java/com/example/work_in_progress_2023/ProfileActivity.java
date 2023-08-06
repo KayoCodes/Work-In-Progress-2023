deb76630bb687cfee7ae12a7075cfc212cfabcfe
DanielSigala <daniel.sigala-aguayo@my.utsa.edu>
2023-08-06 18:28:30 -0500

    test

--- app/src/main/java/com/example/work_in_progress_2023/ProfileActivity.java
+++ app/src/main/java/com/example/work_in_progress_2023/ProfileActivity.java
@@ -1,10 +1,10 @@
 package com.example.work_in_progress_2023;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.ComponentActivity;
 
 import android.os.Bundle;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends ComponentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
