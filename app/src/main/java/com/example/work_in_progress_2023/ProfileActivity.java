d332b3cb81aa2c802ae038289393747ee016f523
jacobschool <brockett04@gmail.com>
2023-08-06 10:15:02 -0500

    created login.txt for authentication and added ProfileActivity.java; will remove once I pull from repo

---
+++ app/src/main/java/com/example/work_in_progress_2023/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.work_in_progress_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
